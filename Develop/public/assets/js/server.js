af012931e79d4b152aac1d24a5a0a6b48215a7d8
ggandara8 <ggandara8@gmail.com>
2020-06-12 21:14:57 -0500

    html routes done using a new js called routes.js

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,4 +1,14 @@
 let express = require("express");
 let app = express();
-let PORT = 4000;
+let PORT = process.env.PORT || 3000;
 let fs = require("fs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/routes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
