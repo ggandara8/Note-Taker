7bf33fd82d5ff4da3096510d3b9b2eb936d269fa
ggandara8 <ggandara8@gmail.com>
2020-06-16 11:42:03 -0500

    notes saving in db.json but not on showing on list

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const notes = require("./Develop/db/db.json");
+const fs = require("fs");
 const direc = path.join(__dirname, "/Develop/public");
 
 let app = express();
@@ -12,16 +13,41 @@ app.use(express.static('Develop/public'));
 
 // HTML routes
 app.get("/", function(req, res) {
-
-res.sendFile(path.join(direc, "index.html"));
-
+    res.sendFile(path.join(direc, "index.html"));
 });
     
 app.get("/notes", function(req, res) {
-
-res.sendFile(path.join(direc, "notes.html"));
-
+    res.sendFile(path.join(direc, "notes.html"));
 });
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(direc, "index.html"));
+});
+
+// Api routes
+app.get("/api/notes", function(req, res) {
+    res.json(notes);
+});
+
+app.post("/api/notes", function(req, res) {
+    notes.push(req.body);
+    addId();
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes));
+});
+
+//Delete obj
+app.delete("/api/notes/:id", function(req, res){
+    notes.splice( notes.findIndex(i => i.id === req.params.id), 1);
+    fs.writeFile("./Develop/db/db.json", JSON.stringify(notes));
+    res.json(notes);
+});
+
+//Add id to objects
+function addId(){
+    notes.forEach((item, i) => {
+        item.id = i + 1;
+    });
+}
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
